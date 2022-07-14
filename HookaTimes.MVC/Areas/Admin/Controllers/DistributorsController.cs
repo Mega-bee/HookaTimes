8cc53a1c6711575091c263846ce082e99e06df6b
Charbel Mahfouz <charbel.mahfouz52@gmail.com>
2022-07-15 01:55:40 +0300

    added new pages

---
+++ HookaTimes.MVC/Areas/Admin/Controllers/DistributorsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HookaTimes.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DistributorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Areas/Admin/Views/Pages/Distributors/Index.cshtml");
+        }
+
+        public IActionResult Distributor()
+        {
+            return View("~/Areas/Admin/Views/Pages/Distributors/Distributor.cshtml");
+        }
+    }
+}
---
+++ HookaTimes.MVC/Areas/Admin/Controllers/HumanResourcesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HookaTimes.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HumanResourcesController : Controller
+    {
+        public IActionResult HookaTeamStaff([FromQuery] string pageName)
+        {
+            ViewBag.PageName = pageName;
+            return View("~/Areas/Admin/Views/Pages/HumanResources/HookaTeamStaff.cshtml");
+        }
+
+        public IActionResult HookaTeamStaffMember()
+        {
+            return View("~/Areas/Admin/Views/Pages/HumanResources/HookaTimesStaffMember.cshtml");
+        }
+
+        public IActionResult HookaTimesTeam()
+        {
+            return View("~/Areas/Admin/Views/Pages/HumanResources/HookaTimesTeam.cshtml");
+        }
+    }
+}
