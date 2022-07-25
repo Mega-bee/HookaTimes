b538b07b071cea99a9fefd97a26f4533b427ac85
Charbel Mahfouz <charbel.mahfouz52@gmail.com>
2022-07-25 12:43:30 +0300

    added hooka buddies

---
+++ HookaTimes.BLL/ViewModels/HookaBuddy_VM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HookaTimes.BLL.ViewModels
+{
+    public partial class HookaBuddy_VM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Image { get; set; }
+    }
+
+    public partial class HookaBuddiesList_VM
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ HookaTimes.MVC/Areas/Admin/Controllers/HookaBuddiesController.cs
@@ -0,0 +1,68 @@
+﻿using HookaTimes.BLL.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HookaTimes.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HookaBuddiesController : Controller
+    {
+
+        public static List<HookaBuddy_VM> hookaBuddies = new List<HookaBuddy_VM>();
+
+        #region Lists
+        private void FillList()
+        {
+            hookaBuddies.Add(new HookaBuddy_VM()
+            {
+                Id = 1,
+                Name = "Hasan Bdeir",
+                PhoneNumber = "+961 123 456",
+                EmailAddress = "h.bdeir@hotmail.com",
+                 Address = "Beirut",
+                  Image = "300-1.jpg",
+                 DateOfBirth = DateTime.UtcNow.ToString(),
+            }); hookaBuddies.Add(new HookaBuddy_VM
+            {
+                Id = 2,
+                Name = "Hadi Bawarshi",
+                PhoneNumber = "+961 123 456",
+                EmailAddress = "hadi.bawarshi@gmail.com",
+                Address = "Beirut",
+                Image = "300-1.jpg",
+                DateOfBirth = DateTime.UtcNow.ToString(),
+
+            });
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            FillList();
+            List<HookaBuddiesList_VM> buddiesList = hookaBuddies.Select(x => new HookaBuddiesList_VM
+            {
+                Id = x.Id,
+                Image = x.Image,
+                Name = x.Name,
+
+            }).ToList();
+            return View("~/Areas/Admin/Views/Pages/HookaBuddies/Index.cshtml", buddiesList);
+        }
+
+        public IActionResult HookaBuddy(int id)
+        {
+            HookaBuddy_VM buddy = hookaBuddies.Where(x=> x.Id == id).Select(x => new HookaBuddy_VM
+            {
+                Id = x.Id,
+                Image = x.Image,
+                Name = x.Name,
+                Address = x.Address,
+                DateOfBirth = x.DateOfBirth,
+                EmailAddress = x.EmailAddress,
+                PhoneNumber = x.PhoneNumber,
+
+            }).FirstOrDefault()!;
+
+            return View("~/Areas/Admin/Views/Pages/HookaBuddies/HookaBuddy.cshtml", buddy);
+        }
+    }
+}
