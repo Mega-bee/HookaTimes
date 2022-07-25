97066c676f220712b618f41f431c1938c3c96f9c
Charbel Mahfouz <charbel.mahfouz52@gmail.com>
2022-07-25 22:17:23 +0300

    added pos-concession, pos-delivery center and pos-restaurants

---
+++ HookaTimes.BLL/ViewModels/Concession_VM.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HookaTimes.BLL.ViewModels
+{
+    public partial class Concession_VM
+    {
+        public int Id { get; set; }
+        public string Dimension { get; set; }
+        public string Capacity { get; set; }
+        public string EmailAddress { get; set; }
+        public string Status { get; set; }
+    }
+
+
+    public partial class ConcessionList_VM
+    {
+        public int Id { get; set; }
+        public string DateJoined { get; set; }
+        public string Status { get; set; }
+    }
+}
