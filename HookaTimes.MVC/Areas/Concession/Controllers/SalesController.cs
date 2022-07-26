2d637e75572f1f82f720bb8db52d47d3dc826774
HadiBawarshi <hadibawarshi@gmail.com>
2022-07-26 21:13:36 +0300

    adding indoor and outdoor pages and their requirements

---
+++ HookaTimes.MVC/Areas/Concession/Controllers/SalesController.cs
@@ -0,0 +1,79 @@
+﻿using HookaTimes.BLL.ViewModels.Concession;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HookaTimes.MVC.Areas.Concession.Controllers
+{
+
+    [Area("Concession")]
+    public class SalesController : Controller
+    {
+        List<SalesList_VM> SalesList = new List<SalesList_VM>();
+
+        #region ListFill
+        private void FillSalesList()
+        {
+            SalesList.Add(new SalesList_VM()
+            {
+                OrderId = 1,
+                OrderDate = DateTime.Now.ToString("dddd, dd MMMM yyyy"),
+                CustomerName = "Hadi Bawarshi",
+                CustomerOrder = "2x Apple Hooka Witty",
+                Total = "USD 30",
+            });
+            SalesList.Add(new SalesList_VM()
+            {
+                OrderId = 2,
+                OrderDate = DateTime.Now.ToString("dddd, dd MMMM yyyy"),
+                CustomerName = "Hadi Bawarshi",
+                CustomerOrder = "2x Apple Hooka Witty",
+                Total = "USD 30",
+            });
+
+            SalesList.Add(new SalesList_VM()
+            {
+                OrderId = 3,
+                OrderDate = DateTime.Now.ToString("dddd, dd MMMM yyyy"),
+                CustomerName = "Hadi Bawarshi",
+                CustomerOrder = "2x Apple Hooka Witty",
+                Total = "USD 30",
+            });
+
+            SalesList.Add(new SalesList_VM()
+            {
+                OrderId = 4,
+                OrderDate = DateTime.Now.ToString("dddd, dd MMMM yyyy"),
+                CustomerName = "Hadi Bawarshi",
+                CustomerOrder = "2x Apple Hooka Witty",
+                Total = "USD 30",
+            });
+        }
+        #endregion
+
+        #region Indoor;                
+        public IActionResult Indoor()
+        {
+            FillSalesList();
+
+            return View("~/Areas/Concession/Views/Pages/Sales/Indoor/Index.cshtml", SalesList);
+        }
+
+        public IActionResult Pos()
+        {
+            FillSalesList();
+
+            return View("~/Areas/Concession/Views/Pages/Sales/Outdoor/Pos.cshtml");
+        }
+
+
+        public IActionResult Outdoor()
+        {
+            FillSalesList();
+
+            return View("~/Areas/Concession/Views/Pages/Sales/Outdoor/Outdoor.cshtml", SalesList);
+        }
+
+
+        #endregion
+
+    }
+}
