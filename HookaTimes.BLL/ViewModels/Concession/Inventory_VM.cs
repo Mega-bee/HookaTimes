2b2d2d1ad80bcd272b676f8186a6ccb153e27b2e
HadiBawarshi <hadibawarshi@gmail.com>
2022-07-27 14:11:23 +0300

    adding page to the inventory controller  OrdersHistory

--- HookaTimes.BLL/ViewModels/Concession/Inventory_VM.cs
+++ HookaTimes.BLL/ViewModels/Concession/Inventory_VM.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HookaTimes.BLL.ViewModels.Concession
+﻿namespace HookaTimes.BLL.ViewModels.Concession
 {
     public partial class Stock_VM
     {
         // public int Id { get; set; }
         public string Product { get; set; }
-        public string Category  { get; set; }
+        public string Category { get; set; }
         public int Quantity { get; set; }
         public string Status { get; set; }
+    }
+
+
+    public partial class Orders_VM
+    {
+        // public int Id { get; set; }
+        public int OrderID { get; set; }
+        public string OrderDate { get; set; }
+        public string OrderDescription { get; set; }
+        public string OrderStatus { get; set; }
     }
 }
---
+++ HookaTimes.MVC/Areas/Concession/Controllers/InventoryController.cs
@@ -0,0 +1,104 @@
+﻿using HookaTimes.BLL.ViewModels.Concession;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HookaTimes.MVC.Areas.Concession.Controllers
+{
+    [Area("Concession")]
+    public class InventoryController : Controller
+    {
+        List<Stock_VM> stock = new List<Stock_VM>();
+        List<Orders_VM> orders = new List<Orders_VM>();
+
+        #region ListFill
+        private void FillStockList()
+        {
+            stock.Add(new Stock_VM()
+            {
+                Product = "Lemon&Mint",
+                Category = "Capsule",
+                Quantity = 23,
+                Status = "Low Qty"
+            });
+            stock.Add(new Stock_VM()
+            {
+                Product = "Lemon&Mint",
+                Category = "Capsule",
+                Quantity = 23,
+                Status = "Low Qty"
+            });
+            stock.Add(new Stock_VM()
+            {
+                Product = "Lemon&Mint",
+                Category = "Capsule",
+                Quantity = 23,
+                Status = "Low Qty"
+            });
+            stock.Add(new Stock_VM()
+            {
+                Product = "Lemon&Mint",
+                Category = "Capsule",
+                Quantity = 23,
+                Status = "Low Qty"
+            });
+
+        }
+
+
+
+
+        private void FillOrderList()
+        {
+            orders.Add(new Orders_VM()
+            {
+                OrderID = 718208,
+                OrderDate = DateTime.Now.ToString("dddd, dd MMMM yyyy"),
+                OrderDescription = "50 Apple Capsule / 30 Grape Cap... ",
+                OrderStatus = "Processing"
+            });
+            orders.Add(new Orders_VM()
+            {
+                OrderID = 718208,
+                OrderDate = DateTime.Now.ToString("dddd, dd MMMM yyyy"),
+                OrderDescription = "50 Apple Capsule / 30 Grape Cap... ",
+                OrderStatus = "Processing"
+            });
+            orders.Add(new Orders_VM()
+            {
+                OrderID = 718208,
+                OrderDate = DateTime.Now.ToString("dddd, dd MMMM yyyy"),
+                OrderDescription = "50 Apple Capsule / 30 Grape Cap... ",
+                OrderStatus = "Processing"
+            });
+            orders.Add(new Orders_VM()
+            {
+                OrderID = 718208,
+                OrderDate = DateTime.Now.ToString("dddd, dd MMMM yyyy"),
+                OrderDescription = "50 Apple Capsule / 30 Grape Cap... ",
+                OrderStatus = "Processing"
+            });
+
+
+
+        }
+        #endregion
+
+        #region Inventory
+
+        public IActionResult Stock()
+        {
+            FillStockList();
+            return View("~/Areas/Concession/Views/Pages/Inventory/Stock/Stock.cshtml", stock);
+        }
+        #endregion
+
+        #region Order
+
+        public IActionResult OrderHistory()
+        {
+            FillOrderList();
+            return View("~/Areas/Concession/Views/Pages/Inventory/OrderHistory/OrderHistory.cshtml", orders);
+        }
+        #endregion
+
+    }
+}
