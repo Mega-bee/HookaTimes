2cac09a16e5eb7d9f3d6720b0a3338a0433e9de7
davidmechelany2001@gmail.com <davidmechelany2001@gmail.com>
2022-07-28 13:16:15 +0300

    Added Concession

---
+++ HookaTimes.BLL/ViewModels/Restaurant/Concession_VM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HookaTimes.BLL.ViewModels.Restaurant
+{
+    public partial class ConcessionsList_VM
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string CustomerName { get; set; }
+        public string Description { get; set; }
+        public string Total { get; set; }
+    }
+
+    public partial class Concession_VM
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string CustomerName { get; set; }
+        public string Description { get; set; }
+        public string Total { get; set; }
+    }
+}
---
+++ HookaTimes.MVC/Areas/Restaurant/Controllers/ConcessionsController.cs
@@ -0,0 +1,95 @@
+﻿using HookaTimes.BLL.ViewModels.Restaurant;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HookaTimes.MVC.Areas.Restaurant.Controllers
+{
+    [Area("Restaurant")]
+    public class ConcessionsController : Controller
+    {
+        public static List<Concession_VM> concessions = new List<Concession_VM>();
+
+        #region Sales List
+        private void FillSales()
+        {
+            concessions.Add(new Concession_VM
+            {
+                Id = 718208,
+                Date = "27 June 2022",
+                CustomerName = "Hasan Bdeir",
+                Description = "2x Apple Hooka Witty",
+                Total = "USD30"
+            });
+            concessions.Add(new Concession_VM
+            {
+                Id = 718207,
+                Date = "26 June 2022",
+                CustomerName = "Imad Al Riz",
+                Description = "3x Lemon&Mint Hooka Witty",
+                Total = "USD45"
+            });
+            concessions.Add(new Concession_VM
+            {
+                Id = 718206,
+                Date = "25 June 2022",
+                CustomerName = "Mariam Meait",
+                Description = "2x Liquorice Hooka Witty",
+                Total = "USD30"
+            });
+            concessions.Add(new Concession_VM
+            {
+                Id = 718205,
+                Date = "24 June 2022",
+                CustomerName = "Badih Abou Hasan",
+                Description = "2x Apple Hooka Witty",
+                Total = "USD30"
+            });
+            concessions.Add(new Concession_VM
+            {
+                Id = 718204,
+                Date = "24 June 2022",
+                CustomerName = "Abir Mansour",
+                Description = "3x Lemon&Mint Hooka Witty",
+                Total = "USD45"
+            });
+            concessions.Add(new Concession_VM
+            {
+                Id = 718203,
+                Date = "23 June 2022",
+                CustomerName = "Hasan Bdeir",
+                Description = "1x Lemon&Mint Hooka Witty ",
+                Total = "USD15"
+            });
+        }
+        #endregion
+
+        public IActionResult Indoor()
+        {
+            FillSales();
+            List<ConcessionsList_VM> concessionList = concessions.Select(x => new ConcessionsList_VM
+            {
+                Id = x.Id,
+                Date = x.Date,
+                CustomerName = x.CustomerName,
+                Description = x.Description,
+                Total = x.Total,
+            }).ToList();
+
+            return View("~/Areas/Restaurant/Views/Pages/Concessions/Indoor.cshtml", concessionList);
+        }
+
+        public IActionResult Outdoor()
+        {
+            FillSales();
+            List<ConcessionsList_VM> concessionList = concessions.Select(x => new ConcessionsList_VM
+            {
+                Id = x.Id,
+                Date = x.Date,
+                CustomerName = x.CustomerName,
+                Description = x.Description,
+                Total = x.Total,
+            }).ToList();
+
+            return View("~/Areas/Restaurant/Views/Pages/Concessions/Outdoor.cshtml", concessionList);
+        }
+    }
+}
