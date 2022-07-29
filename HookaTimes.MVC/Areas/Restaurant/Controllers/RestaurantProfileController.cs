bb2d448ba9a1f86cdeb2c894ef747229202b9baa
HadiBawarshi <hadibawarshi@gmail.com>
2022-07-29 10:08:51 +0300

    adding restaurant profile pages

---
+++ HookaTimes.MVC/Areas/Restaurant/Controllers/RestaurantProfileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HookaTimes.MVC.Areas.Restaurant.Controllers
+{
+    [Area("Restaurant")]
+    public class RestaurantProfileController : Controller
+    {
+
+        public IActionResult EditProfile()
+        {
+            return View("~/Areas/Restaurant/Views/Pages/RestaurantProfile/RestaurantProfile.cshtml");
+        }
+    }
+}
