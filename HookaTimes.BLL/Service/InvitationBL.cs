65ecd2de55499eaca83337ad487b33c0f3af96b6
Charbel Mahfouz <charbel.mahfouz52@gmail.com>
2022-08-05 15:41:14 +0300

    added invitation service

---
+++ HookaTimes.BLL/Service/InvitationBL.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HookaTimes.BLL.Utilities;
+using HookaTimes.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HookaTimes.BLL.Service
+{
+    public class InvitationBL : BaseBO
+    {
+        public InvitationBL(IUnitOfWork unit, IMapper mapper, NotificationHelper notificationHelper) : base(unit, mapper, notificationHelper)
+        {
+        }
+    }
+}
