053f187d1e298d775228870e27457422a7a4aa26
HadiBawarshi <hadibawarshi@gmail.com>
2022-08-16 20:59:39 +0300

    adding edit profile

--- HookaTimes.BLL/ViewModels/Website/Authurization_VM.cs
+++ HookaTimes.BLL/ViewModels/Website/Authurization_VM.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HookaTimes.BLL.ViewModels.Website
 {
@@ -69,4 +71,38 @@ namespace HookaTimes.BLL.ViewModels.Website
         [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
+
+
+
+    public partial class CompleteProfileMVC_VM
+    {
+        public IFormFile ImageFile { get; set; }
+        //public string Image { get; set; }
+        public string AboutMe { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int GenderId { get; set; }
+        public int MaritalStatus { get; set; }
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+        public int BodyType { get; set; }
+        public int Eyes { get; set; }
+        public int Hair { get; set; }
+        //public int Education { get; set; }
+        public string Profession { get; set; }
+        public string Interests { get; set; }
+        public string Hobbies { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SocialMediaLink1 { get; set; }
+        public string SocialMediaLink2 { get; set; }
+        public string SocialMediaLink3 { get; set; }
+        //public List<BuddyProfileAddressPutVM> Addresses { get; set; }
+        //public List<BuddyProfileEducationPutVM> Education { get; set; }
+        //public List<BuddyProfileExperiencePutVM> Experience { get; set; }
+
+
+    }
+
+
+
 }
