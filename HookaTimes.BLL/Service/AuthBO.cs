659150f8183485f4854f706e6644d0b187034ac6
HadiBawarshi <hadibawarshi@gmail.com>
2022-08-18 10:51:58 +0300

    fixing the image path in create buddy profile

--- HookaTimes.BLL/Service/AuthBO.cs
+++ HookaTimes.BLL/Service/AuthBO.cs
@@ -519,7 +519,7 @@ namespace HookaTimes.BLL.Service
             newProfile.FirstName = model.FirstName;
             newProfile.LastName = model.LastName;
             newProfile.IsAvailable = true;
-            newProfile.Image = "Images/Buddies/user-placeholder.png";
+            newProfile.Image = "/Images/Buddies/user-placeholder.png";
             //newProfile.GenderId = 1;
             // add the characteristics to the BuddyProfiles
             await _context.BuddyProfiles.AddAsync(newProfile);
