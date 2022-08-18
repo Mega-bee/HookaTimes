ef72b9a818e53c566369925a6b8a4597f340c6ca
Charbel Mahfouz <charbel.mahfouz52@gmail.com>
2022-08-18 15:59:15 +0300

    fixes

--- HookaTimes.BLL/ViewModels/Website/HookaProduct_VM.cs
+++ HookaTimes.BLL/ViewModels/Website/HookaProduct_VM.cs
@@ -12,6 +12,7 @@ namespace HookaTimes.BLL.ViewModels.Website
         public string CategoryTitle { get; set; }
         public string CategoryImage { get; set; }
         public List<HookaProduct_VM> Products { get; set; }
+        public List<Product_VM> RelatedCategories { get; set; }
 
     }
 
@@ -23,5 +24,12 @@ namespace HookaTimes.BLL.ViewModels.Website
         public decimal? CustomerFinalPrice { get; set; }
         public string Description { get; set; }
         public bool IsInWishlist { get; set; }
+    }
+
+    public partial class RelatedCategory_VM
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string Image { get; set; }
     }
 }
